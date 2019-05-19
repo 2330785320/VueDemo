d3abcd42f5ad7e7e95eb2d337d4a07f4c73f525f
little fatty <2330785320@qq.com>
2019-05-20 07:35:25 +0800

    vmodel使用

---
+++ 5.19.5/js/main.js
@@ -0,0 +1,10 @@
+var app = new new Vue({
+    el: "#app",
+    data: {
+       name: "名称",
+       price: 10
+    },
+    methods: {
+        
+    }
+});
---
+++ 5.19.6/js/main.js
@@ -0,0 +1,14 @@
+var app = new new Vue({
+    el: "#app",
+    data: {
+       name: "名称",
+       sex: "female",
+       hobby: ["female"],
+       remark: "这是一个长字符串",
+       from: 1,
+       dest: [1,2]
+    },
+    methods: {
+        
+    }
+});
